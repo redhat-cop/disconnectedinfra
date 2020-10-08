aa90e75b92a83d4fcece5a75755d7208dcf849b4
Holden <htfenner@onondaga.io>
2020-10-08 17:31:28 -0400

    Migrated vars to inventory and rebuilt packerfile

---
+++ setup.sh
@@ -0,0 +1,6 @@
+export VMWARE_USER=administrator@vsphere.infrabuild.xyz
+export VMWARE_PASSWORD=BadPassword1234$
+export VMWARE_HOST=192.168.5.1
+export VMWARE_VALIDATE_CERTS=no
+export VSPHERE_ALLOW_UNVERIFIED_SSL=true
+export PACKER_CACHE_DIR=/build_artifact
