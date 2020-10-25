e3e7c82aa97410a63b45aac2e82aec8fe0805b03
Holden <htfenner@onondaga.io>
2020-10-25 13:32:27 -0400

    Split setup vars with VMWare default vars

--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
-export VMWARE_USER=administrator@vsphere.infrabuild.xyz
-export VMWARE_PASSWORD=BadPassword1234$
-export VMWARE_HOST=192.168.5.1
-export VMWARE_VALIDATE_CERTS=no
-export VSPHERE_ALLOW_UNVERIFIED_SSL=true
 export PACKER_CACHE_DIR=/build_artifact
-export GOVC_USERNAME=${VMWARE_USER}
-export GOVC_PASSWORD=${VMWARE_PASSWORD}
-export GOVC_URL=${VMWARE_HOST}
-export GOVC_INSECURE=True
+export ANSIBLE_ROLES_PATH=./roles/:../roles:~/.ansible/roles:/usr/share/ansible/roles:/etc/ansible/roles
+
