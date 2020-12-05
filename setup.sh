d34b571e86da7f9f3a428fb01f5f305a0b384d9a
Holden Fenner <hfenner@redhat.com>
2020-12-05 09:22:24 -0500

    Resolved Ansible packer issue

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 export PACKER_CACHE_DIR=/build_artifact
 export ANSIBLE_ROLES_PATH=./roles/:../roles:~/.ansible/roles:/usr/share/ansible/roles:/etc/ansible/roles
+export PACKER_LOG=1
+export PACKER_LOG_PATH=/build_artifact/packerlog.txt
 
